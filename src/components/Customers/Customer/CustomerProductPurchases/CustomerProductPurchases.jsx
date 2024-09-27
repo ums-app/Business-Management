df81043482430e515cf5d48436a5682684f781c7
aliherawi <aliherawi7@gmail.com>
2024-09-27 10:41:08 +0430

    index number added

--- src/components/Customers/Customer/CustomerProductPurchases/CustomerProductPurchases.jsx
+++ src/components/Customers/Customer/CustomerProductPurchases/CustomerProductPurchases.jsx
@@ -271,6 +271,7 @@ function CustomerProductPurchases() {
                     <thead >
                         <tr>
                             <th>{t('number')}</th>
+                            <th>{t('indexNumber')}</th>
                             <th>{t('name')}</th>
                             <th>{t('lastName')}</th>
                             <th>{t('createdDate')}</th>
@@ -298,6 +299,7 @@ function CustomerProductPurchases() {
                                 key={factor.id}
                             >
                                 <td>{index + 1}</td>
+                                <td>{factor.indexNumber}</td>
                                 <td>{factor?.customer?.name}</td>
                                 <td>{factor?.customer?.lastName}</td>
                                 <td>{factor.createdDate && gregorianToJalali(new Date(factor?.createdDate.toDate())).join('/')} </td>
