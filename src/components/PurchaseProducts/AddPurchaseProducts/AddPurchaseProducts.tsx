f8eb248bfcf9ec9f92a450056ccbab35786f26a5
aliherawi <aliherawi7@gmail.com>
2024-10-06 23:03:13 +0430

    delete added

--- src/components/PurchaseProducts/AddPurchaseProducts/AddPurchaseProducts.tsx
+++ src/components/PurchaseProducts/AddPurchaseProducts/AddPurchaseProducts.tsx
@@ -1,9 +1,9 @@
 import { t } from 'i18next';
 import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Button from '../../UI/Button/Button';
 import { Product, UpdateModeProps } from '../../../Types/Types';
-import { collection, doc, getCountFromServer, runTransaction, Timestamp, writeBatch } from 'firebase/firestore';
+import { collection, deleteDoc, doc, getCountFromServer, runTransaction, Timestamp, writeBatch } from 'firebase/firestore';
 import ICONS from '../../../constants/Icons';
 import { getProducts } from '../../../Utils/FirebaseTools';
 import CustomDatePicker from '../../UI/DatePicker/CustomDatePicker';
@@ -39,6 +39,7 @@ export interface PurchaseFactor {
 
 const AddPurchaseProducts: React.FC<UpdateModeProps> = ({ updateMode = false }) => {
     const nav = useNavigate();
+    const { purchaseProductId } = useParams();
     const [{ PurchaseFactor }, dispatch] = useStateValue()
     const [purchaseFactor, setPurchaseFactor] = useState<PurchaseFactor>(updateMode ? PurchaseFactor : {
         products: [{
@@ -270,10 +271,10 @@ const AddPurchaseProducts: React.FC<UpdateModeProps> = ({ updateMode = false }) 
             return;
         }
 
-        // dispatch({
-        //     type: actionTypes.SET_SMALL_LOADING,
-        //     payload: true
-        // });
+        dispatch({
+            type: actionTypes.SET_SMALL_LOADING,
+            payload: true
+        });
         const totalAmount = summarize('totalAll')
         const totalProduct = summarize('totalProduct')
         const totalPackage = summarize('package')
@@ -284,16 +285,18 @@ const AddPurchaseProducts: React.FC<UpdateModeProps> = ({ updateMode = false }) 
             totalProducts: totalProduct,
             totalPackage: totalPackage
         }
-
         setPurchaseFactor(factorTemp)
 
         try {
 
             // Using transaction to add the first document
             const factorDocRef = doc(collection(db, Collections.Purchases)); // Assuming you are using 'sales' collection
-            await runTransaction(db, async (transaction) => {
+            const result = await runTransaction(db, async (transaction) => {
                 transaction.set(factorDocRef, factorTemp); // Set the factor data in the transaction
             });
+
+            console.log("transction result: ", result);
+
 
             // Call the update function, which will also use a transaction
             await updateMultipleDocuments(purchaseFactor.products);
@@ -336,7 +339,46 @@ const AddPurchaseProducts: React.FC<UpdateModeProps> = ({ updateMode = false }) 
         }
     }
 
-    const deletePurchaseFactor = () => {
+
+    const showDeleteModal = () => {
+        dispatch({
+            type: actionTypes.SHOW_ASKING_MODAL,
+            payload: {
+                show: true,
+                message: "deleteMessage",
+                btnAction: deletePurchaseFactor,
+            },
+        });
+    };
+
+    const deletePurchaseFactor = async () => {
+        if (!updateMode) return
+
+        dispatch({
+            type: actionTypes.SET_GLOBAL_LOADING,
+            payload: { value: true },
+        });
+        dispatch({
+            type: actionTypes.HIDE_ASKING_MODAL,
+        });
+
+
+        if (!purchaseProductId) return;
+
+        const factorDoc = doc(db, Collections.Purchases, purchaseProductId);
+
+        try {
+            await deleteDoc(factorDoc)
+            toast.success(t('successfullyDeleted'));
+            nav("/purchase-products")
+        } catch (err) {
+            console.log(err);
+        } finally {
+            dispatch({
+                type: actionTypes.SET_GLOBAL_LOADING,
+                payload: { value: false },
+            });
+        }
 
     }
 
@@ -353,14 +395,11 @@ const AddPurchaseProducts: React.FC<UpdateModeProps> = ({ updateMode = false }) 
                         text={t('readyForPrint')}
                         onClick={() => setshowPrintModal(true)}
                     />} */}
-                    <Button
+                    {updateMode && <Button
                         icon={ICONS.trash}
                         text={t("delete")}
-                        onClick={() =>
-
-                            toast.error('notImplementedYet')
-                        }
-                    />
+                        onClick={showDeleteModal}
+                    />}
                 </Menu>
 
                 <Button
@@ -398,67 +437,69 @@ const AddPurchaseProducts: React.FC<UpdateModeProps> = ({ updateMode = false }) 
                         </span>
                     </div>
                 </div>
+                <div className='overflow_x_scroll'>
+                    <table className='custom_table full_width'>
+                        <thead>
+                            <tr>
+                                <th>{t('number')}</th>
+                                <th>{t("name")}</th>
+                                <th>{t("total")} {t('package')}</th>
+                                <th>{t('total')}</th>
+                                <th>{t('pricePer')}</th>
+                                <th>{t('customsFees')}</th>
+                                <th>{t('additionalCosts')}</th>
+                                <th>{t("totalAll")}</th>
+                                <th>{t("actions")}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {purchaseFactor?.products.map((item, index) => {
+                                return (
+                                    <tr>
+                                        <td>{index + 1}</td>
+                                        <td>
+                                            <select name="" id=""
+                                                onChange={(e) => handleProductChange(e, index)}
+                                                style={{ minWidth: '160px' }}
+                                            >
+                                                <option value={undefined}>{t("chooseTheProduct")}</option>
+                                                {products.map(pr => {
+                                                    return <option
+                                                        selected={item.productId == pr.id} style={{ width: 'max-content' }}
+                                                        disabled={checkIfProductIsInList(pr.id)}
+                                                        value={pr.id}
+                                                    >
+                                                        {pr.name}
+                                                    </option>
+                                                })}
+                                            </select>
+                                        </td>
+                                        <td><input type="number" name="" style={{ width: '100px' }} value={item.totalPackage} onChange={e => handleChangeTotalPackage(e, index)} /></td>
+                                        <td><input type="number" name="" style={{ width: '100px' }} value={item.totalNumber} onChange={e => handleChangeTotalNumber(e, index)} /></td>
+                                        <td><input type="number" name="" style={{ width: '100px' }} value={item.pricePer} onChange={e => handleChangePricePer(e, index)} /></td>
+                                        <td><input type="number" name="" style={{ width: '100px' }} value={item.customsCosts} onChange={e => handleChangeCustomsCosts(e, index)} /></td>
+                                        <td><input type="number" name="" style={{ width: '100px' }} value={item.additionalCosts} onChange={e => handleChangeAdditionalCosts(e, index)} /></td>
+                                        <td>{item.total}</td>
+                                        <td>
+                                            <Button
+                                                icon={ICONS.trash}
+                                                onClick={() => removeRow(index)}
+                                            />
+                                        </td>
+                                    </tr>
+                                )
+                            })}
 
-                <table className='custom_table full_width'>
-                    <thead>
-                        <tr>
-                            <th>{t('number')}</th>
-                            <th>{t("name")}</th>
-                            <th>{t("total")} {t('package')}</th>
-                            <th>{t('total')}</th>
-                            <th>{t('pricePer')}</th>
-                            <th>{t('customsFees')}</th>
-                            <th>{t('additionalCosts')}</th>
-                            <th>{t("totalAll")}</th>
-                            <th>{t("actions")}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {purchaseFactor?.products.map((item, index) => {
-                            return (
-                                <tr>
-                                    <td>{index + 1}</td>
-                                    <td>
-                                        <select name="" id=""
-                                            onChange={(e) => handleProductChange(e, index)}
-                                        >
-                                            <option value={undefined}>{t("chooseTheProduct")}</option>
-                                            {products.map(pr => {
-                                                return <option
-                                                    selected={item.productId == pr.id} style={{ width: 'max-content' }}
-                                                    disabled={checkIfProductIsInList(pr.id)}
-                                                    value={pr.id}
-                                                >
-                                                    {pr.name}
-                                                </option>
-                                            })}
-                                        </select>
-                                    </td>
-                                    <td><input type="number" name="" style={{ width: '100px' }} value={item.totalPackage} onChange={e => handleChangeTotalPackage(e, index)} /></td>
-                                    <td><input type="number" name="" style={{ width: '100px' }} value={item.totalNumber} onChange={e => handleChangeTotalNumber(e, index)} /></td>
-                                    <td><input type="number" name="" style={{ width: '100px' }} value={item.pricePer} onChange={e => handleChangePricePer(e, index)} /></td>
-                                    <td><input type="number" name="" style={{ width: '100px' }} value={item.customsCosts} onChange={e => handleChangeCustomsCosts(e, index)} /></td>
-                                    <td><input type="number" name="" style={{ width: '100px' }} value={item.additionalCosts} onChange={e => handleChangeAdditionalCosts(e, index)} /></td>
-                                    <td>{item.total}</td>
-                                    <td>
-                                        <Button
-                                            icon={ICONS.trash}
-                                            onClick={() => removeRow(index)}
-                                        />
-                                    </td>
-                                </tr>
-                            )
-                        })}
 
 
-
-                    </tbody>
-                    <Button
-                        icon={ICONS.plus}
-                        btnType=' margin_top_10'
-                        onClick={addNewRow}
-                    />
-                </table>
+                        </tbody>
+                        <Button
+                            icon={ICONS.plus}
+                            btnType=' margin_top_10'
+                            onClick={addNewRow}
+                        />
+                    </table>
+                </div>
 
 
                 <table className='custom_table full_width margin_top_20'>
